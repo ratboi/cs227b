0889f1ef38ab3d0dc15bac6fb9448872b70c2ee4
Owner-PC <Owner@Toshiba.(none)>
2009-04-22 01:46:09 -0700

    new easy gamer class

---
+++ ggp/src/player/gamer/statemachine/reflex/easy/EasyGamer.java
@@ -0,0 +1,75 @@
+package player.gamer.statemachine.reflex.easy;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import apps.player.detail.DetailPanel;
+
+import player.gamer.statemachine.StateMachineGamer;
+import player.gamer.statemachine.reflex.event.ReflexMoveSelectionEvent;
+import player.gamer.statemachine.reflex.gui.ReflexDetailPanel;
+import util.statemachine.Move;
+import util.statemachine.StateMachine;
+import util.statemachine.MachineState;
+import util.statemachine.Role;
+import util.statemachine.exceptions.GoalDefinitionException;
+import util.statemachine.exceptions.MoveDefinitionException;
+import util.statemachine.exceptions.TransitionDefinitionException;
+import util.statemachine.prover.cache.CachedProverStateMachine;
+
+public class EasyGamer extends StateMachineGamer {
+
+	@Override
+	public void stateMachineMetaGame(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		// Do nothing.
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		long start = System.currentTimeMillis();
+		StateMachine stateMachine = getStateMachine();
+		MachineState currentState = getCurrentState();
+		Role role = getRole();
+		Map<Role, Integer> roleIndices = stateMachine.getRoleIndices();
+
+		List<Move> legalMoves = stateMachine.getLegalMoves(currentState, role);
+		List<List<Move>> moves = stateMachine.getLegalJointMoves(currentState);
+		
+		int maxGoal = 0;
+		Move selection = null;
+		
+		for (List<Move> move : moves) {
+			MachineState nextState = stateMachine.getNextState(currentState, move);
+			int goal = stateMachine.getGoal(nextState, role);
+			if (goal >= maxGoal) {
+				maxGoal = goal;
+				selection = move.get(roleIndices.get(role));
+			}
+		}
+		
+		long stop = System.currentTimeMillis();
+
+		notifyObservers(new ReflexMoveSelectionEvent(legalMoves, selection, stop - start));
+		return selection;
+	}
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new CachedProverStateMachine();
+	}
+
+	@Override
+	public String getName() {
+		return "Easy";
+	}
+
+	@Override
+	public DetailPanel getDetailPanel() {
+		return new ReflexDetailPanel();
+	}
+
+	
+}
