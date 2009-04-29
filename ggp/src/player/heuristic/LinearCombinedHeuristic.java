898d228024c5d08f4b2058b8717af5c3bad6d132
Owner-PC <Owner@Toshiba.(none)>
2009-04-29 00:44:22 -0700

    combined heuristic

--- ggp/src/player/heuristic/LinearCombinedHeuristic.java
+++ ggp/src/player/heuristic/LinearCombinedHeuristic.java
@@ -7,18 +7,27 @@ import util.statemachine.exceptions.GoalDefinitionException;
 import util.statemachine.exceptions.MoveDefinitionException;
 import util.statemachine.exceptions.TransitionDefinitionException;
 
+import java.util.List;
+
 public class LinearCombinedHeuristic implements Heuristic {
+	
+	private List<Heuristic> heuristics;
+	private List<Double> weights;
+	
+	public LinearCombinedHeuristic(List<Heuristic> heuristics, List<Double> weights) {
+		this.heuristics = heuristics;
+		this.weights = weights;
+	}
 	
 	@Override
 	public double eval(StateMachine stateMachine, MachineState state, Role role)
 			throws MoveDefinitionException, TransitionDefinitionException,
 			GoalDefinitionException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	public void addHeuristic(Heuristic h) {
-		
+		double totalScore = 0.0;
+		for (int i = 0; i < heuristics.size(); i++) {
+			totalScore += weights.get(i) * heuristics.get(i).eval(stateMachine, state, role);
+		}
+		return totalScore;
 	}
 
 }
